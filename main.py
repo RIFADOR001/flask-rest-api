14da6cb6dc3fa7c83c1d6659afad182f7bc815e2
rifador001 <rifador001@gmail.com>
2024-05-18 18:40:36 -0600

    db integration is complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, fields, marshal_with, abort
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -25,31 +25,66 @@ video_put_args.add_argument("name", type=str, help="Name of the video is require
 video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes on the video is required", required=True)
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name oof the video")
+video_update_args.add_argument("views", type=int, help="Views on the video")
+video_update_args.add_argument("likes", type=int, help="Likes on the video")
 
-videos = {}
-
-def abort_video_doesnt_exist(video_id):
-	if video_id not in videos:
-		abort(404, message="Video not found...")
-
-def abort_video_exists(video_id):
-	if video_id in videos:
-		abort(409, message="Video already exists...")
+resource_fields = {
+	'id': fields.Integer,
+	'name': fields.String,
+	'views': fields.Integer,
+	'likes': fields.Integer
+}
 
 class Video(Resource):
+	# With this decorator, the return value is transform and
+	# serialize (transform into dictionary) with the indicated parameters
+	# It modifies the json but the status code is the same
+	@marshal_with(resource_fields)
 	def get(self, video_id):
-		abort_video_doesnt_exist(video_id)
-		return videos[video_id]		
+		# Result is an instances of VideoModel class
+		result = VideoModel.query.filter_by(id=video_id).first()
+		if not result:
+			abort(404, message="Could not find video with that id")
+		return result
 
+	@marshal_with(resource_fields)
 	def put(self, video_id):
-		abort_video_exists(video_id)
+		result = VideoModel.query.filter_by(id=video_id).first()
+		print('hi')
+		if result:
+			abort(409, message="Video id taken...")
 		args = video_put_args.parse_args()
-		videos[video_id] = args
-		return videos[video_id], 201
+		video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+		db.session.add(video) # This will add the object
+		db.session.commit()
+		return video, 201
+
+	@marshal_with(resource_fields)
+	def patch(self, video_id):
+		args = video_update_args.parse_args()
+		result = VideoModel.query.filter_by(id=video_id).first()
+		if not result:
+			abort(404, message="Could not find video with that id")
+		if args['name']:
+			result.name = args['name']
+		if args['views']:
+			result.views = args['views']
+		if args['likes']:
+			result.likes = args['likes']
+
+		db.session.commit()
+
+		return result
+
 
 	def delete(self, video_id):
-		abort_video_doesnt_exist(video_id)
-		del videos[video_id]
+		result = VideoModel.query.filter_by(id=video_id).first()
+		if not result:
+			abort(404, message="Could not find video with that id")
+		db.session.delete(result)
+		db.session.commit()
 		return '', 204
 
 
--- test.py
+++ test.py
@@ -4,30 +4,41 @@ BASE = "http://127.0.0.1:5000/"
 
 data = [{"likes":i, "name": "Tim "+str(i), "views":1000*i} for i in range(1, 10)]
 
-response = requests.put(BASE + "video/1", json={"likes": 10, "name": "10", "views": 11})
+response = requests.put(BASE + "video/55", json={"likes": 10, "name": "10", "views": 11})
 # response = requests.put(BASE + "video/1", json={"likes": 10, "name": "10", "views": 11}, headers={"Content-Type":"application/json"})
 # response = requests.put(BASE + "video/1", json={"likes": 10}, headers={"Content-Type":"application/json"})
-for i in range(len(data)):
-	response = requests.put(BASE + "video/" + str(i), json=data[i])
-	print(response.json())
-# try:
-#     print(response.json())
-# except Exception as e:
-#     print(f'caught {type(e)}: ' + e)
+# for i in range(len(data)):
+# 	response = requests.put(BASE + "video/" + str(i), json=data[i])
+# 	print(response.json())
+print(response)
+try:
+    print(response.json())
+except Exception as e:
+    print(f'caught {type(e)}: ' + e)
 
 input()
-for i in range(len(data)):
-	response = requests.get(BASE + "video/" + str(i))
-	print(response.json())
+response = requests.get(BASE + "video/55")
+print(response.json())
+
+response = requests.patch(BASE + "video/55", json={"views": 99})
+# response = requests.get(BASE + "video/55")
+print(response.json())
+
+response = requests.delete(BASE + "video/55")
+print(response)
+
+# for i in range(len(data)):
+# 	response = requests.get(BASE + "video/" + str(i))
+# 	print(response.json())
 
 # response = requests.get(BASE + "video/1")
 # print(response.json())
 
-response = requests.delete(BASE + "video/5")
-print(response)
-for i in range(len(data)):
-	response = requests.get(BASE + "video/" + str(i))
-	print(response.json())
+# response = requests.delete(BASE + "video/5")
+# print(response)
+# for i in range(len(data)):
+# 	response = requests.get(BASE + "video/" + str(i))
+# 	print(response.json())
 # try:
 #     print(response.json())
 # except Exception as e:
